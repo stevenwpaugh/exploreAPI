81c4a368a36942232ab8d66fd29fcaf324f54f76
Steven W. Paugh <steven.paugh@stjude.org>
2012-11-21 14:25:36 -0600

    Modified PCGP.R in preparation for version 0.3

--- PCGP.R
+++ PCGP.R
@@ -1,17 +1,11 @@
-.getPCGPemail <- function () {
-if (!exists(".PCGPemail")){
-.PCGPemail <<- readline("Please enter your PCGP registered email address: \n")
-}
-}
-
 pcgpinstall <- function(x){
 if (x == "PCGPDataAPI"){
 .getPCGPemail()
-download.file(paste("http://explore.pediatriccancergenomeproject.org/PCGPDataAPI_0.2.tar.gz?email=", .PCGPemail, sep=""), paste(getwd(), "PCGPDataAPI_0.1.tar.gz", sep="/"), mode="wb")
+download.file(paste("http://explore.pediatriccancergenomeproject.org/PCGPDataAPI_0.3.tar.gz"), paste(getwd(), "PCGPDataAPI_0.3.tar.gz", sep="/"), mode="wb")
 install.packages("XML")
 install.packages("png")
-install.packages(paste(getwd(), "PCGPDataAPI_0.2.tar.gz", sep="/"), repos=NULL, type="source")
-unlink(paste(getwd(), "PCGPDataAPI_0.2.tar.gz", sep="/"))
+install.packages(paste(getwd(), "PCGPDataAPI_0.3.tar.gz", sep="/"), repos=NULL, type="source")
+unlink(paste(getwd(), "PCGPDataAPI_0.3.tar.gz", sep="/"))
 }
 if (x != "PCGPDataAPI"){
 cat("ERROR: An invalid package name was specified\nPlease try again\nSee http://explore.pediatriccancergenomeproject.org/apis/ for more details.\n")
