43488d33f21e9ad5892b5a1e585bf209078ca4e1
Steven Paugh <steven.paugh@stjude.org>
2012-11-28 12:55:51 -0600

    Edited development mode for getPCGPbaseURL.R

--- PCGPDataAPI/R/getPCGPbaseURL.R
+++ PCGPDataAPI/R/getPCGPbaseURL.R
@@ -1,5 +1,7 @@
 getPCGPbaseURL <- function(){
+if (!exists("dev.test")){
 return ("http://explore.pediatriccancergenomeproject.org/")
+}
 if (exists("dev.test")){
 return ("http://pcgpdtportaldev01.stjude.org/")
 }
