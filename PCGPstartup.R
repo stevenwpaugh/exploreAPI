2ae88ba84ca6515f5ff5aec815b7b9572e50d7cc
Steven Paugh <steven.paugh@stjude.org>
2012-12-31 15:31:31 -0600

    Updated PCGP.R and PCGPstartup.R for 0.4

--- PCGPstartup.R
+++ PCGPstartup.R
@@ -1,14 +1,18 @@
 PCGPDataAPIStartup <- function(packagever){
 
 if (packagever == "PCGPDataAPI_0.1"){
-cat("Welcome to Explore! \n Please visit us at\n\n http://explore.pediatriccancergenomeproject.org/ \n\nfor version 0.3 of the API.\n")
+cat("Welcome to Explore! \n Please visit us at\n\n http://explore.pediatriccancergenomeproject.org/ \n\nfor version 0.4 of the API.\n")
 }
 
 if (packagever == "PCGPDataAPI_0.2"){
-cat("Welcome to Explore! \n Please visit us at\n\n http://explore.pediatriccancergenomeproject.org/ \n\nfor version 0.3 of the API.\n")
+cat("Welcome to Explore! \n Please visit us at\n\n http://explore.pediatriccancergenomeproject.org/ \n\nfor version 0.4 of the API.\n")
 }
 
 if (packagever == "PCGPDataAPI_0.3"){
+cat("Welcome to Explore! \n Please visit us at\n\n http://explore.pediatriccancergenomeproject.org/ \n\nfor version 0.4 of the API.\n")
+}
+
+if (packagever == "PCGPDataAPI_0.4"){
 cat("Welcome to Explore!\n")
 }
 
