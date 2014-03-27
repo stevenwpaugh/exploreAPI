b68f6c3a34c01a68d2e9d30cd44a01bf909fc723
stevepaugh <stevepaugh@gmail.com>
2014-03-27 17:03:23 -0500

    Replaced cat with packageStartupMessage in onLoad function

--- PCGPDataAPI/R/onLoad.R
+++ PCGPDataAPI/R/onLoad.R
@@ -1,23 +1,12 @@
 .onLoad <-function(libname, pkgname){
-  cat("Using PCGPDataAPI version 0.4\n")
-  
+  packageStartupMessage("Using PCGPDataAPI version 0.4\n")
   file.name <- paste(sample (letters, 6), collapse="")
-  
-  
-  
   try (download.file("http://explore.pediatriccancergenomeproject.org/PCGPstartup.R", paste(getwd(), file.name, sep="/")), silent=TRUE) 
-  
   if (grepl("PCGPstartup.R", geterrmessage())){
-    
-    cat("Warning: unable to connect to http://explore.pedatriccancergenomeproject.org/\n Proceeding to load PCGPDataAPI, but connection might be inactive\n\n")
-    
+    packageStartupMessage("Warning: unable to connect to http://explore.pedatriccancergenomeproject.org/\n Proceeding to load PCGPDataAPI, but connection might be inactive\n\n")
   }
-  
   if (!grepl("PCGPstartup.R", geterrmessage())){
-    
-    
     source("http://explore.pediatriccancergenomeproject.org/PCGPstartup.R")
-    
     PCGPDataAPIStartup("PCGPDataAPI_0.4")
   }
   unlink(paste(getwd(), file.name, sep="/"))
