265f81b3c6a42ccb1c4788749d85a23579bcf325
Steven W. Paugh <stevepaugh@gmail.com>
2014-03-27 21:41:39 -0500

    Removed extra space

--- PCGPstartup.R
+++ PCGPstartup.R
@@ -1,19 +1,14 @@
 PCGPDataAPIStartup <- function(packagever){
-
   if (packagever == "PCGPDataAPI_0.1"){
-    cat("Welcome to Explore! \n Please visit us at\n\n http://explore.pediatriccancergenomeproject.org/ \n\nfor version 0.4 of the API.\n")
+    cat("Welcome to Explore!\nPlease visit us at\n\nhttp://explore.pediatriccancergenomeproject.org/\n\nfor version 0.4 of the API.\n")
   }
-  
   if (packagever == "PCGPDataAPI_0.2"){
-    cat("Welcome to Explore! \n Please visit us at\n\n http://explore.pediatriccancergenomeproject.org/ \n\nfor version 0.4 of the API.\n")
+    cat("Welcome to Explore!\nPlease visit us at\n\nhttp://explore.pediatriccancergenomeproject.org/\n\nfor version 0.4 of the API.\n")
   }
-  
   if (packagever == "PCGPDataAPI_0.3"){
-    cat("Welcome to Explore! \n Please visit us at\n\n http://explore.pediatriccancergenomeproject.org/ \n\nfor version 0.4 of the API.\n")
+    cat("Welcome to Explore!\nPlease visit us at\n\nhttp://explore.pediatriccancergenomeproject.org/\n\nfor version 0.4 of the API.\n")
   }
-  
   if (packagever == "PCGPDataAPI_0.4"){
     cat("Welcome to Explore!\n")
   }
-  
 }
